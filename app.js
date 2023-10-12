f183d6e0bc0f17f68b52953d7d849696eba920b4
mujahed-hash <mujahedahmed786@gmail.com>
2023-10-12 16:29:13 -0600

    slug update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const categoryRoute = require('./routes/category');
 var path = require('path');
 
 
-app.use(cors())
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
